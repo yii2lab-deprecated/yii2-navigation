7ea434d4f2b399ead874ba807da88db5ad05af64
Vitaliy <theyamshikov@yandex.ru>
2018-03-30 15:29:28 +0600

    make docblock in domain

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -4,6 +4,14 @@ namespace yii2lab\navigation\domain;
 
 use yii2lab\domain\enums\Driver;
 
+/**
+ * Class Domain
+ *
+ * @package yii2lab\navigation\domain
+ *
+ * @property \yii2lab\navigation\domain\services\BreadcrumbsService $breadcrumbs
+ * @property \yii2lab\navigation\domain\services\AlertService $alert
+ */
 class Domain extends \yii2lab\domain\Domain {
 	
 	public function config() {
