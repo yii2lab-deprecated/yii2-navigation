397a219cf1431c4393a915e6200c635ac2f1f5ec
Vitaliy <theyamshikov@yandex.ru>
2018-03-31 20:34:00 +0600

    clean

--- src/domain/interfaces/services/BreadcrumbsInterface.php
+++ src/domain/interfaces/services/BreadcrumbsInterface.php
@@ -5,5 +5,7 @@ namespace yii2lab\navigation\domain\interfaces\services;
 use yii2lab\domain\interfaces\services\CrudInterface;
 
 interface BreadcrumbsInterface extends CrudInterface {
-
+	
+	public function create($title, $url = null, $options = null);
+	
 }
